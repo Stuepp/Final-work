b269835be88a4868149a56eeab49acb2fe4bafe5
Paulo <paulo.henrique@jetbov.com>
2023-11-29 22:56:15 -0300

    cria edit avaliation

--- gain_tracker/training/urls.py
+++ gain_tracker/training/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('newhistory/', views.SaveNewHistory.as_view(), name='newhistory'),
     path('edittraining/<int:id>/', views.EditTraining.as_view(), name='edittraining'),
     path('editexercise/<int:id>/', views.EditExercise.as_view(), name='editexercise'),
+    path('editavaliation/<int:id>/', views.EditAvaliation.as_view(), name='editavaliation'),
 
 ]
